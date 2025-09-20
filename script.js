42b9f123cebe2d37608f22c7d53e04121fa24460
Yash708262 <yash708262@gmail.com>
2025-09-20 16:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toFahrenheit(celsius) {
- // Write your code here
+  const celsiusNum = Number(celsius); 
+
+  if (isNaN(celsiusNum)) {
+    return "Invalid input: Not a number";
+  }
+  if (celsiusNum < -273.15 || celsiusNum > 1e9) {
+    return "Invalid input: Out of range";
+  }
+
+  result =  (celsiusNum * 9 / 5) + 32;
+  return result.toFixed(2)
 }
 
 // Do not change the code below
